8cfe486e095be8d4c0592e298ad3aebe33b0bb85
Salvatore Calo <dafder45678@gmail.com>
2025-12-15 07:51:24 +0000

    fix: removed useless duplicated command delete_instagram_directory

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,12 +5,10 @@ from .logger import setup_logger
 from .file_operations import save_bytes_to_file, clean_directory
 from .image_processing import create_long_image
 from .convert_admin_ids import convert_admin_ids
-from .clean_download_directory import clean_download_directory
 __all__ = [
     'setup_logger',
     'save_bytes_to_file',
     'clean_directory',
     'create_long_image',
     "convert_admin_ids",
-    "clean_download_directory"
 ]
