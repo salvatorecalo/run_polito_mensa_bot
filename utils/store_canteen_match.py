f36e3e820937ad03cf22efb3e796a148deea4980
Salvatore Calo <dafder45678@gmail.com>
2026-01-20 15:44:39 +0000

    fix: improved image processing

--- utils/store_canteen_match.py
+++ utils/store_canteen_match.py
@@ -1,7 +1,6 @@
 from database.models import Canteen
 from utils.get_canteen_keywords import get_canteen_keywords
 
-
 def store_canteen_match(text: str, canteen: Canteen):
     keywords = get_canteen_keywords(canteen)
     score = 0
