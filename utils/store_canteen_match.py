582cbb70105ecb8c9773c95cde0c5fcded3faa78
Salvatore Calo <dafder45678@gmail.com>
2026-01-22 10:21:26 +0000

    fix: update readme, add missing imports in __init__ e fix wrong langage model name and store canteen feature to support all canteens name estraction by the ocr

--- utils/store_canteen_match.py
+++ utils/store_canteen_match.py
@@ -2,28 +2,6 @@ from database.models import Canteen
 from utils.get_canteen_keywords import get_canteen_keywords
 from utils import normalize_text
 
-# def store_canteen_match(text: str, canteen: Canteen):
-#     text_norm = normalize_text(text)
-#     name_norm = normalize_text(canteen.name)
-    
-#     name_no_spaces = name_norm.replace(" ", "")
-    
-#     # If the exact canteen name is in the text, give high score
-#     if name_no_spaces in text:
-#         return 10
-    
-#     # Fallback to keyword matching
-#     keywords = get_canteen_keywords(canteen)
-#     score = 0
-#     for kw in keywords:
-#         if kw in text_norm:
-#             score += 1
-#     return score
-    
-from database.models import Canteen
-from utils.get_canteen_keywords import get_canteen_keywords
-from utils import normalize_text
-
 def store_canteen_match(text: str, canteen: Canteen):
     text_norm = normalize_text(text)
     name_norm = normalize_text(canteen.name)
