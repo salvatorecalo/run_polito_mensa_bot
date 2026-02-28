793f08308786a5a89a3227c6d2deccf49f8366e4
Salvatore Calo <dafder45678@gmail.com>
2026-02-28 10:57:15 +0000

    fix: removed creation of file bot.db because it causes linux permission reading errors

--- utils/setup_data_folder.py
+++ utils/setup_data_folder.py
@@ -1,5 +1,4 @@
 import os
-from pathlib import Path
 from .logger import setup_logger
 
 logger = setup_logger(__name__)
@@ -10,8 +9,6 @@ def setup_data_folder():
         os.makedirs("download", exist_ok=True)
         os.makedirs("download/stories", exist_ok=True)
         os.makedirs("download/created_images", exist_ok=True)
-        os.makedirs("data", exist_ok=True)
-        Path("data/bot.db").touch()
         logger.info("Cartelle data and download create con successo")
     except Exception as e:
         logger.error(e)
